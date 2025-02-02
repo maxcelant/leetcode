61b35ab103e80d0e068d87415d0da667d7990d40
Max Celant <maxcelant@gmail.com>
2025-02-02 13:45:49 -0600

    new problems

--- arrays/medium_top-k-most-frequent-elements.py
+++ arrays/medium_top-k-most-frequent-elements.py
@@ -6,19 +6,21 @@ from typing import List
 
 class Solution:
   def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    # Determine frequency of each number
     freq = defaultdict(int) 
     for n in nums:
       freq[n] += 1
     
-    heap = []
+    heap = [] # Heaps are min-heap by default
     for n in freq.keys():
       heapq.heappush(heap, (freq[n], n))
       if len(heap) > k:
-        heapq.heappop(heap)
+        heapq.heappop(heap) # Popping the top of the heap (smallest)
 
-    print(heap)
-    
+    return [n for (_, n) in heap]
+
+
 nums = [1,2,2,3,3,3]
 k = 2
 s = Solution()
-s.topKFrequent(nums, k)
+print(s.topKFrequent(nums, k))
