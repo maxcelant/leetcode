4addfcfb92e39a9bc09a967a5d105b44d586c79e
Max Celant <maxcelant@gmail.com>
2025-02-05 15:47:08 -0600

    more problems

---
+++ arrays/medium/longest-consecutive-sequence.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        seq = set(nums)
+        cnt = 0
+        for n in nums: 
+            tmp_cnt = 0
+            # If a larger number in the sequence exists
+            # just skip this one.
+            if n+1 in seq: continue
+            cur = n
+            # Loop downwards until all the values are captured
+            while cur in seq:
+                cur =- 1
+                tmp_cnt += 1
+            cnt = max(cnt, tmp_cnt)
+
+        return cnt
+
+s = Solution()
+print(s.longestConsecutive([2,20,4,10,3,4,5]))
