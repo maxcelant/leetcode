74741b54c7b820c7a990ad62f072228f92b5043d
Max Celant <massimiliano.celant@aa.com>
2025-02-06 13:12:52 -0600

    two pointer

--- two_pointers/medium/3-sum.py
+++ two_pointers/medium/3-sum.py
@@ -1,4 +1,36 @@
+from typing import List
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        
+        for i in range(len(nums)):
+            # If the current number is greater than 0, we can't find a triplet summing to 0
+            if nums[i] > 0: 
+                break
+            # Skip duplicate numbers to avoid duplicate triplets
+            if i > 0 and nums[i] == nums[i - 1]: 
+                continue  
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                target = nums[l] + nums[r] + nums[i]
+                if target == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    # Skip duplicates for l
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    # Skip duplicates for r
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif target < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return res
 
+s = Solution()
+print(s.threeSum([-1,0,1,2,-1,-4]))
---
+++ two_pointers/medium/container-with-most-water.py
@@ -0,0 +1,23 @@
+
+
+'''
+- The volume of the water will be the diff between the pointers X the min of the two heights
+'''
+
+
+class Solution:
+    def maxArea(self, heights: list[int]) -> int:
+        res = 0
+        l, r = 0, len(heights) - 1
+        while l < r:
+            diff = r - l
+            area = diff * min(heights[l], heights[r])
+            res = max(area, res)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+
+s = Solution()
+print(s.maxArea([1,7,2,5,4,7,3,6]))
