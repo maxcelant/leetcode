7eb0eef9087359fe56bf97f1fbab5e48841d5b50
Max Celant <massimiliano.celant@aa.com>
2025-02-09 09:56:33 -0600

    adding new problem

---
+++ sliding_window/medium/longest-substring-without-repeating-characters.py
@@ -0,0 +1,24 @@
+
+'''
+- Use sliding window, add characters to a set
+- Check if the character on the right you are adding is already in the set,
+- if it is, then we remove from the left until its not in the set.
+'''
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window = set() 
+        l, r = 0, 0
+        count = 0
+        while r <= len(s) - 1:
+            while s[r] in window:
+                window.remove(s[l])
+                l += 1
+            window.add(s[r])
+            r += 1
+            count = max(count, len(window))
+        return count
+
+
+
